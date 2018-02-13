d1171ed8abeee50015673ffca309f8c9968ce109
Amit <singhatmich@gmail.com>
2018-02-13 15:08:40 +0530

    added drawer layout

--- app/src/main/java/com/apps/devamit/gyanshala/MainActivity.java
+++ app/src/main/java/com/apps/devamit/gyanshala/MainActivity.java
@@ -1,9 +1,13 @@
 package com.apps.devamit.gyanshala;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
@@ -13,19 +17,44 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private ActionBar actionBar;
+
     private static final int RC_SIGN_IN = 123;
     private static FirebaseUser thisUser;
     private DatabaseReference mDatabase;
-    List<AuthUI.IdpConfig> providers = Arrays.asList(
+    List<AuthUI.IdpConfig> providers = Collections.singletonList(
             new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDrawerLayout= findViewById(R.id.drawer_layout);
+        mDrawerToggle=new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_opened, R.string.drawer_closed) {
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+        actionBar=getSupportActionBar();
+        if(actionBar!=null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeButtonEnabled(true);
+            mDrawerToggle.syncState();
+        }
+
         thisUser=FirebaseAuth.getInstance().getCurrentUser();
         //if this User==null the user hasn't signed in even once
         if(thisUser==null) {
@@ -40,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void loggedIn() {
         Toast.makeText(this, "the current user is : " + thisUser.getDisplayName(), Toast.LENGTH_SHORT).show();
+
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("gyanshala-hackathon");
         myRef.setValue("let's get this done");
