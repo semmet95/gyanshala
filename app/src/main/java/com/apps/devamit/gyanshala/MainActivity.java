b61182463c7ff57c70a869af41a547f7417131dc
Amit <singhatmich@gmail.com>
2018-02-13 16:16:12 +0530

    drawer layout set

--- app/src/main/java/com/apps/devamit/gyanshala/MainActivity.java
+++ app/src/main/java/com/apps/devamit/gyanshala/MainActivity.java
@@ -1,15 +1,22 @@
 package com.apps.devamit.gyanshala;
 
 import android.content.Intent;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -24,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mDrawerToggle;
     private ActionBar actionBar;
+    private NavigationView navigationView;
+    private View nav_header_main;
 
     private static final int RC_SIGN_IN = 123;
     private static FirebaseUser thisUser;
@@ -48,12 +57,16 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         mDrawerLayout.addDrawerListener(mDrawerToggle);
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
         actionBar=getSupportActionBar();
         if(actionBar!=null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setHomeButtonEnabled(true);
             mDrawerToggle.syncState();
         }
+        navigationView=findViewById(R.id.navigation);
+        nav_header_main=getLayoutInflater().inflate(R.layout.nav_header_main, navigationView, false);
+        navigationView.addHeaderView(nav_header_main);
 
         thisUser=FirebaseAuth.getInstance().getCurrentUser();
         //if this User==null the user hasn't signed in even once
@@ -67,12 +80,29 @@ public class MainActivity extends AppCompatActivity {
             loggedIn();
     }
 
+    //call when the user has logged in
     private void loggedIn() {
         Toast.makeText(this, "the current user is : " + thisUser.getDisplayName(), Toast.LENGTH_SHORT).show();
+        Glide.with(this).load(thisUser.getPhotoUrl()).into((ImageView)nav_header_main.findViewById(R.id.userImage));
+        ((TextView)nav_header_main.findViewById(R.id.userName)).setText(thisUser.getDisplayName());
+        ((TextView)nav_header_main.findViewById(R.id.userEmail)).setText(thisUser.getEmail());
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("gyanshala-hackathon");
         myRef.setValue("let's get this done");
+    }
+
+    @Override
+    public void onPostCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onPostCreate(savedInstanceState, persistentState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mDrawerToggle.onOptionsItemSelected(item))
+            return true;
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
