5029085004b707a03c5cec661b5f3b5de0607904
Amit <singhatmich@gmail.com>
2018-02-17 19:42:46 +0530

    Added Activities.

--- app/src/main/java/com/apps/devamit/gyanshala/MainActivity.java
+++ app/src/main/java/com/apps/devamit/gyanshala/MainActivity.java
@@ -58,6 +58,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
